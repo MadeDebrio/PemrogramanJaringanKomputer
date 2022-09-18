b16a7723dad9e4f02d7c8b2de936ee3a40e46192
I Made Debrio Amarta <debrioamarta@gmail.com>
2022-09-19 01:46:04 +0700

    EncryptionDecription

---
+++ TASK3/encrypt_decrypt.php
@@ -0,0 +1,47 @@
+<?php
+  
+// Input username
+$Username = readline('Username: ');
+// Input password
+$password = readline('Password: ');
+
+// Store cipher method
+$ciphering = "aes-128-cbc";
+  
+// Use OpenSSl encryption method to gets the cipher initialization vector (iv) length.
+//return value is a length in bytes
+$iv_length = openssl_cipher_iv_length($ciphering);
+$options = 0;
+  
+// random_bytes() function gives randomly 16 digit values
+$encryption_iv = random_bytes($iv_length);
+  
+//open ssl digest Computes a digest hash value for the given data using a given method, 
+//and returns a raw or binhex encoded string.
+// php uname returns information about the operating system PHP is running on
+$encryption_key = openssl_digest(php_uname(), 'MD5', TRUE);
+  
+// Encryption of string process starts
+$encryption = openssl_encrypt($password, $ciphering,
+        $encryption_key, $options, $encryption_iv);
+  
+// Display the encrypted string
+echo "Username: " . $Username. "\n";
+echo "Encrypted String: " . $encryption . "\n";
+  
+
+// random_bytes() function gives randomly 16 digit values
+$decryption_iv = random_bytes($iv_length);
+  
+// Store the decryption key
+$decryption_key = openssl_digest(php_uname(), 'MD5', TRUE);
+  
+// Decryption of string process starts
+$decryption = openssl_decrypt ($encryption, $ciphering,
+            $decryption_key, $options, $encryption_iv);
+  
+// Display the decrypted string
+echo "Username: " . $Username. "\n";
+echo "Decrypted String: " . $decryption;
+  
+?>
